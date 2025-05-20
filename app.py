6e2f5f6ce24b7d9570ed6cb91380759cbcfbb8cb
Poshangg <165940225+Poshangg@users.noreply.github.com>
2025-05-20 13:32:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1439,7 +1439,5 @@ def submit_collector_report():
         return redirect(url_for('collector_reports'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-port = int(os.enviro.get("PORT",5000))
-app.run(host="0.0.0.0", port =port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
